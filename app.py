9e114583ce141eefd868f55e51fe82a828f32d95
Jazmin Capuli Yuen <jaz@jazmins-air.lan>
2022-06-17 14:51:13 -0700

    Set up the database in app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-app = Flask(__name__)
+from flask import Flask, jsonify
 
-@app.route('/')
-
-@app.route('/')
-def hello_world():
-    return 'Hello world'
-
+# Access the SQLite database
+engine = create_engine("sqlite:///hawaii.sqlite")
+# Reflect the database into our classes
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create a session link from Python to the database
+session = Session(engine)
